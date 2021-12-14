0c671fbb58029a93d2783943b328d8d0c5450189
abanoub <abanoub.edwar13@gmail.com>
2021-12-14 12:18:23 +0200

    introduce crash

--- app/src/main/java/com/example/sentrypoc/one/FirstActivity1.kt
+++ app/src/main/java/com/example/sentrypoc/one/FirstActivity1.kt
@@ -5,14 +5,18 @@ import android.os.Handler
 import android.os.Looper
 import com.example.sentrypoc.BaseActivity
 import com.example.sentrypoc.R
-import java.lang.RuntimeException
+import java.lang.IllegalArgumentException
+import java.lang.IllegalStateException
 
 class FirstActivity1 : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
         Handler(Looper.getMainLooper()).postDelayed({
-            throw RuntimeException("Islam should handle this crash")
+            crash3()
         }, 1000)
     }
+
+    fun crash2(): Unit = throw IllegalStateException("Islam should handle this different crash");
+    fun crash3(): Unit = throw IllegalArgumentException("Islam should handle this different new crash");
 }
