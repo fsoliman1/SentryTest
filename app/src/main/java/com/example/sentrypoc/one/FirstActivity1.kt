de76fad1cf78c03e19a18d833ad4bfdcaf77a4a2
abanoub <abanoub.edwar13@gmail.com>
2021-12-22 12:34:53 +0200

    BugFix2

--- app/src/main/java/com/example/sentrypoc/one/FirstActivity1.kt
+++ app/src/main/java/com/example/sentrypoc/one/FirstActivity1.kt
@@ -5,16 +5,18 @@ import android.os.Handler
 import android.os.Looper
 import com.example.sentrypoc.BaseActivity
 import com.example.sentrypoc.R
+import java.lang.ArithmeticException
 import java.lang.IllegalArgumentException
 import java.lang.IllegalStateException
 import java.lang.NumberFormatException
+import java.lang.RuntimeException
 
 class FirstActivity1 : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
         Handler(Looper.getMainLooper()).postDelayed({
-            crash5()
+            crash6()
         }, 1000)
     }
 
@@ -22,4 +24,5 @@ class FirstActivity1 : BaseActivity() {
     fun crash3(): Unit = throw IllegalArgumentException("Islam should handle this different new crash");
     fun crash4(): Unit = throw IllegalThreadStateException("Islam should handle this different brand new crash");
     fun crash5(): Unit = throw NumberFormatException("Islam should handle this different brand new second crash");
+    fun crash6(): Unit = throw ArithmeticException("Islam should handle this different brand new second crash");
 }
